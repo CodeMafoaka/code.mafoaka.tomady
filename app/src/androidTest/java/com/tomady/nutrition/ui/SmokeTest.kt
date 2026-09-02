b4774b7a5ef2e40c7ada18afddb7057aee7e9112
Daniel Langio <langio.tehiniavo@gmail.com>
2026-09-03 00:07:28 +0300

    fix: missing TOMADY_TABS import in SmokeTest.kt

--- app/src/androidTest/java/com/tomady/nutrition/ui/SmokeTest.kt
+++ app/src/androidTest/java/com/tomady/nutrition/ui/SmokeTest.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.tomady.nutrition.ui.components.TOMADY_TABS
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
